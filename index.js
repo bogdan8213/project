a301b8b6ff6dc95aa61ef2faedec83c0d97ff5c3
Artem <tema@it4p.ru>
2021-10-22 16:40:51 +0300

    up

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
-let letter = document.querySelector(letter)
+
+let words = [{
+  text: 'Б..чёвка',
+  variants: ['и', 'е'],
+  correct: 'и'
+}, {
+  text: 'Б..чёвка2',
+  variants: ['и', 'е', 'у', 'd'],
+  correct: 'и'
+}, {
+  text: 'Б..чёвка3',
+  variants: ['и', 'е', 'г'],
+  correct: 'и'
+}];
+
+const $wordText = document.getElementById('word-text');
+const $wordsCount = document.getElementById('words-count');
+const $wordVariants = document.getElementById('word-variants');
+
+const App = {
+  startIndex: 0,
+  setNextWord() {
+    const nextWord = words[this.startIndex];
+    $wordText.innerHTML = nextWord.text;
+    $wordsCount.innerHTML = this.startIndex;
+    $wordVariants.innerHTML = '';
+    nextWord.variants.forEach((item, index) => {
+      const $variantEl = document.createElement('button');
+      $variantEl.innerHTML = item;
+      $variantEl.onclick = () => {
+        if (item === nextWord.correct) {
+          this.isCorrect();
+        } else {
+          this.isWrong();
+        }
+      }
+      $wordVariants.appendChild($variantEl);
+    });
+  },
+
+  isCorrect() {
+    alert('Правильно');
+    this.startIndex++;
+    if (words[this.startIndex]) {
+      this.setNextWord();
+    } else {
+      alert('Конец');
+    }
+  },
+
+  isWrong() {
+    alert('Неверно');
+  }
+}
+
+App.setNextWord();
+
+
