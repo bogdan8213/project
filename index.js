601cb8c1a6d37b22c74eb7796eca059e91059c8b
Artem <tema@it4p.ru>
2021-11-04 12:41:38 +0300

    up

--- index.js
+++ index.js
@@ -1,68 +1,86 @@
 
-let words = [{
-  text: 'Б..чёвка',
-  variants: ['и', 'е'],
-  correct: 'и'
-}, {
-  text: 'крыж..вник',
-  variants: ['о', 'е', 'ё'],
-  correct: 'ё'
-}, {
-  text: 'ш..пот',
-  variants: ['о', 'ё'],
-  correct: 'ё'
-},
-  {
-    text: 'реш..тка',
-    variants: ['о', 'ё'],
-    correct: 'ё'
-},
-  {
-    text: 'кап..шон',
-    variants: ['е', 'ю'],
-    correct: 'ю'
-  }];
-
-const $wordText = document.getElementById('word-text');
-const $wordsCount = document.getElementById('words-count');
-const $wordVariants = document.getElementById('word-variants');
-
 const App = {
+  words: [],
+  $wordText: document.getElementById('word-text'),
+  $wordsCount: document.getElementById('words-count'),
+  $wordVariants: document.getElementById('word-variants'),
   startIndex: 0,
+
+  start() {
+    this.fetchData();
+  },
+
+  fetchData() {
+    // загружаем слова
+    fetch('./data.json')
+      .then(res => res.json())
+      .then(out => {
+        this.words = out;
+        console.log('Data: ', out)
+        this.loadProgress();
+        this.setNextWord();
+      });
+  },
+
   setNextWord() {
-    const nextWord = words[this.startIndex];
-    $wordText.innerHTML = nextWord.text;
-    $wordsCount.innerHTML = this.startIndex;
-    $wordVariants.innerHTML = '';
+    const nextWord = this.words[this.startIndex];
+    this.$wordText.innerHTML = nextWord.text;
+    this.$wordsCount.innerHTML = this.startIndex;
+    this.$wordVariants.innerHTML = '';
     nextWord.variants.forEach((item, index) => {
       const $variantEl = document.createElement('button');
       $variantEl.innerHTML = item;
+      $variantEl.classList.add('btn', 'btn-outline-primary');
       $variantEl.onclick = () => {
         if (item === nextWord.correct) {
           this.isCorrect();
         } else {
           this.isWrong();
         }
+        this.saveProgress();
       }
-      $wordVariants.appendChild($variantEl);
+      this.$wordVariants.appendChild($variantEl);
     });
   },
 
   isCorrect() {
-    alert('Правильно');
     this.startIndex++;
-    if (words[this.startIndex]) {
+    if (this.words[this.startIndex]) {
       this.setNextWord();
     } else {
+      // TODO: выводить сообщение текстом рядом с кнопкой вместо alert
       alert('Конец');
     }
   },
 
   isWrong() {
+    // TODO: выводить сообщение текстом рядом с кнопкой вместо alert
     alert('Неверно');
+  },
+
+  // сохраняем в хранилище
+  // TODO: сохранять количество ошибок
+  saveProgress() {
+    localStorage.startIndex = this.startIndex;
+  },
+
+  // загружаем из хранилища
+  // TODO: загружать количество ошибок
+  loadProgress() {
+    if (localStorage.startIndex) {
+      this.startIndex = +localStorage.startIndex;
+    }
+    console.log('this.startIndex', this.startIndex);
+  },
+
+  // TODO: сбросить прогресс и начать заново
+  resetProgress() {
+
   }
 }
 
-App.setNextWord();
+App.start();
+
+
 
 
